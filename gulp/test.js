e89594ef55bf4985c8fbd9552e99a16cb04957e9
Michael Hendren <michael.hendren@gmail.com>
2015-03-01 16:04:43 -0600

    Added support for specifying the reporter on the command line, useful for specifying xunit, which jenkins uses.

--- gulp/test.js
+++ gulp/test.js
@@ -3,8 +3,11 @@
  */
 var gulp = require('gulp');
 var mocha = require('gulp-mocha-co');
+var minimist = require('minimist');
+
+var options = minimist(process.argv.slice(2), {default: {reporter: 'spec'}});
 
 gulp.task('test', function() {
     gulp.src('test/**/*-test.js')
-        .pipe(mocha({reporter: 'list'}));
+        .pipe(mocha({reporter: options.reporter}));
 });
