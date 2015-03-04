af23bc67949c033ad6a3e4b3873d0d86275e0da5
Michael Hendren <mrhendren@yahoo.com>
2015-03-04 11:58:59 -0600

    Factored out a log of common code, and uncurried places where it was making it harder to understand.

--- src/js/SHA256.js
+++ src/js/SHA256.js
@@ -31,56 +31,53 @@ module.exports = function(message) {
     function iter(msg, h) {
         function schedule(depth, wi) {
             var n = 48 - depth;
+            var ww = WORD(wi);
 
             function sCompute(w0, s1, s2, s3) {
-                return compose(XOR(ROR(w0)(s1)), XOR(ROR(w0)(s2)))(SHR(w0)(s3));
+                var RORw = ROR(w0);
+                return apply(XOR, RORw(s1), RORw(s2), SHR(w0, s3));
             }
 
-            var s0 = sCompute(WORD(wi)(n+1), 7, 18, 3);
-            var s1 = sCompute(WORD(wi)(n+14), 17, 19, 10);
-            return depth == 0 ? wi : schedule(depth - 1, wi.concat(apply(ADD, WORD(wi)(n), s0, s1, WORD(wi)(n+9))));
+            var s0 = sCompute(ww(n+1), 7, 18, 3);
+            var s1 = sCompute(ww(n+14), 17, 19, 10);
+            return depth == 0 ? wi : schedule(depth - 1, wi.concat(apply(ADD, ww(n), s0, s1, ww(n+9))));
         }
 
         function compress(depth, maxDepth, w, a, b, c, d, e, f, g, j) {
 
             var pos = maxDepth - depth;
+            var wh = WORD(h);
 
             function sCompute(v, s1, s2, s3) {
-                return apply(XOR, ROR(v)(s1), ROR(v)(s2), ROR(v)(s3));
+                var RORv = ROR(v);
+                return apply(XOR, RORv(s1), RORv(s2), RORv(s3));
             }
 
             function genTemp1() {
                 var s1 = sCompute(e, 6, 11, 25);
-                var ch = XOR(AND(e)(f))(AND(NOT(e))(g));
+                var ch = XOR(AND(e, f), AND(NOT(e), g));
                 return apply(ADD, j, s1, ch, WORD(k)(pos), WORD(w)(pos));
             }
 
             function genTemp2() {
                 var s0 = sCompute(a, 2, 13, 22);
-                var maj = apply(XOR, AND(a)(b), AND(a)(c), AND(b)(c));
+                var maj = apply(XOR, AND(a, b), AND(a, c), AND(b, c));
                 return ADD(s0)(maj);
             }
 
             function genHash() {
-                return apply(cconcat, ADD(WORD(h)(0))(a), ADD(WORD(h)(1))(b), ADD(WORD(h)(2))(c), ADD(WORD(h)(3))(d),
-                    ADD(WORD(h)(4))(e), ADD(WORD(h)(5))(f), ADD(WORD(h)(6))(g), ADD(WORD(h)(7))(j));
+                return apply(cconcat, ADD(wh(0), a), ADD(wh(1), b), ADD(wh(2), c), ADD(wh(3), d),
+                    ADD(wh(4), e), ADD(wh(5), f), ADD(wh(6), g), ADD(wh(7), j));
             }
 
-            return depth == 0 ? genHash() : compress(depth - 1, maxDepth, w, ADD(genTemp1())(genTemp2()), a,
-                b, c, ADD(d)(genTemp1()), e, f, g);
+            return depth == 0 ? genHash() : compress(depth - 1, maxDepth, w, ADD(genTemp1(), genTemp2()), a,
+                b, c, ADD(d, genTemp1()), e, f, g);
         }
 
-        var a0 = WORD(h)(0);
-        var b0 = WORD(h)(1);
-        var c0 = WORD(h)(2);
-        var d0 = WORD(h)(3);
-        var e0 = WORD(h)(4);
-        var f0 = WORD(h)(5);
-        var g0 = WORD(h)(6);
-        var j0 = WORD(h)(7);
+        var wh = WORD(h);
 
         return msg.length == 0 ? h : iter(msg.slice(512), compress(64, 64, schedule(48, msg.slice(0, 512)),
-            a0, b0, c0, d0, e0, f0, g0, j0));
+            wh(0), wh(1), wh(2), wh(3), wh(4), wh(5), wh(6), wh(7)));
     }
 
     return binary.toHex(iter(message, h0));
